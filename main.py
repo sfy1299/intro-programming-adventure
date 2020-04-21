ab82bd7ba42615dcc2414d62256f7b7b84f033be
Sarah Gao <sfy1299@yahoo.com>
2020-04-20 22:33:19 -0700

    Based on feedback, removed duplicate code. Simplify the logic.

--- main.py
+++ main.py
@@ -4,28 +4,6 @@ import random
 # IMPORTS
 
 # PARAMETERS
-min = input("Enter the minimum value: ")
-max = input("Enter the maximum value: ")
-try:
-    min = int(min)
-    max = int(max)
-except TypeError:
-    print("You didn't enter the right values, so 1 as min and 6 as max.")
-    min = 1
-    max = 6
-except ValueError:
-    print("You didn't enter the right values, so 1 as min and 6 as max.")
-    min = 1
-    max = 6
-
-if min > max:
-    print("You didn't enter the right values, so 1 as min and 6 as max.")
-    min = 1
-    max = 6
-else:
-    print("Thanks for entering the correct inputs!")
-
-# PARAMETERS
 
 
 # UTILITY FUNCTIONS
@@ -39,218 +17,87 @@ def random_choice_func(min, max):
 
 
 def get_play_again():
-    print_sleep("Would you like to play again?", 0.2)
-    print_sleep("Enter 0 for no and 1 for yes:")
-    try:
-        play = int(input("Play again [0/1] > "))
-        if play == 0:
-            played = False
-        elif play == 1:
-            played = True
-        else:
-            print("I don't know that, so bye!")
-            exit()
-        return played
-    except TypeError:
-        print("Invalid input, so exiting. Bye!")
+    print_sleep("Would you like to play again?")
+    print_sleep("Enter 1 for no\nEnter 2 for yes")
+    play = int(input("(Please enter 1 or 2.)\n"))
+
+    if play == 1:
         exit()
-    except ValueError:
-        print("Invalid input, so exiting. Bye!")
-        exit()
+    else:
+        game()
 # UTILITY FUNCTIONS
 
 
 # RANDOMIZED VARIABLES
-random_choice = random_choice_func(min, max)
-random_test = random_choice_func(min, max)
+random_choice = random_choice_func(1, 6)
+random_test = random_choice_func(1, 6)
 # RANDOMIZED VARIABLES
 
+
 # GAME FUNCTION
-
-
 def game():
     if random_choice <= random_test:
+        print_sleep("You find yourself standing in a field, with wildflowers.")
         print_sleep(
-            """
-            You find yourself standing in a field, with grass and wildflowers.
-            """)
-        print_sleep(
-            """
-            Rumor has that gorgon somewhere here, and terrifying the village.
-            """)
+            "Rumor has that gorgon somewhere here, and terrifying the village."
+        )
         print_sleep("In front of you is a house.")
         print_sleep("To your right is a dark cave.")
         print_sleep(
-            """
-            In your hand you hold your trusty (but not very effective) dagger.
-            """)
-        print_sleep("\nEnter 0 to knock on the door of the house.")
-        print_sleep("Enter 1 to peer into the cave.")
-        inputd = input("Enter 0 or 1 [0/1] > ")
-        try:
-            inputd = int(inputd)
-        except TypeError:
-            print(
-                "Wrong input, I'm asking you if you want to play again!")
-            result = get_play_again()
-            if result:
-                game()
-            else:
-                exit()
-        except ValueError:
-            print(
-                "Wrong input, I'm asking you if you want to play again!")
-            result = get_play_again()
-            if result:
-                game()
-            else:
-                exit()
-        if inputd == 0:
+            "In your hand you hold your trusty (but not effective) dagger."
+        )
+        print_sleep(
+            "\nEnter 1 to knock on the door.\nEnter 2 to go into the cave."
+        )
+        inputd = int(input("(Please enter 1 or 2.)\n"))
+        if inputd == 1:
             print_sleep("You are about to knock and the troll attacks you!")
             print_sleep(
-                "Since you died, I'm asking you if you want to play again!")
-            result = get_play_again()
-            if result:
-                game()
-            else:
-                exit()
-        elif inputd == 1:
-            print_sleep(
-                """
-                Oh no! You died! :( Asking you if you want to play again.
-                """)
-            result = get_play_again()
-            if result:
-                game()
-            else:
-                exit()
+                "Since you died, I'm asking you if you want to play again!"
+            )
+            get_play_again()
         else:
             print_sleep(
-                """
-                Wrong input, asking you if you want to play again.
-                """)
-            result = get_play_again()
-            if result:
-                game()
-            else:
-                exit()
+                "Oh no! You died! :( Asking you if you want to play again."
+            )
+            get_play_again()
 
     else:
         print_sleep("You find yourself in a dark dungeon.")
         print_sleep("In your pocket is a shiny Swiss Knife.")
         print_sleep("In the cave, you hear angry bears screaming.")
-        try:
-            response_choice = int(input(
-                "Stay in cave, enter 0, get out of cave, enter 1: > "))
-        except TypeError:
-            print("""
-            Wrong input, asking you if you want to play again.
-            """)
-            result = get_play_again()
-        except ValueError:
-            print(
-                "Wrong input, I'm asking you if you want to play again!")
-            result = get_play_again()
-            if result:
-                game()
-            else:
-                exit()
+        print_sleep("Enter 1 to stay in cave\nEnter 2 to get out of cave.")
+        response_choice = int(input("(Please enter 1 or 2.)\n"))
 
-        if response_choice == 0:
+        if response_choice == 1:
             print_sleep("Great choice! You found an awesome sword!")
-            print_sleep(
-                """
-                Exit cave or build a shelter in the cave?
-                """)
-            try:
-                response_choice = int(
-                    input("Enter 0 to exit cave or 1 to build shelter > "))
-            except TypeError:
-                print_sleep("Oops! You didn't enter the right input!")
-                result = get_play_again()
-                if result:
-                    game()
-                else:
-                    exit()
-            except ValueError:
-                print(
-                    "Wrong input, asking if you want to play again!")
-                result = get_play_again()
-                if result:
-                    game()
-                else:
-                    exit()
-            if response_choice == 0:
-                print_sleep("""
-                Oops, since the cave exit is too far away, you're stranded!
-                """)
-                result = get_play_again()
-                if result:
-                    game()
-                else:
-                    exit()
-            elif response_choice == 1:
-                print_sleep("""
-                The bears found you, you're stuck in the cave forever!
-                """)
-                result = get_play_again()
-                if result:
-                    game()
-                else:
-                    exit()
-            else:
-                response_MESSAGE = False
-                while (response_MESSAGE is False):
-                    try:
-                        response_choice = int(
-                            input("0 to exit cave or 1 to build shelter > "))
-                        if response_choice == 0:
-                            print_sleep("""
-                                The cave exit is far away, you're stranded!
-                                """)
-                            result = get_play_again()
-                            if result:
-                                game()
-                            else:
-                                exit()
-                        elif response_choice == 1:
-                            print_sleep("""
-                            Bears found you, stuck in the cave forever!
-                            """)
-                            result = get_play_again()
-                            if result:
-                                game()
-                            else:
-                                exit()
-                        else:
-                            print("Invalid input, so bye!")
-                            exit()
+            print_sleep("Exit cave or build a shelter in the cave?")
+            print_sleep("Enter 1 to exit cave\nEnter 2 to build shelter.")
+            response_choice = int(input("(Please enter 1 or 2.)\n"))
 
-                    except TypeError:
-                        print_sleep("Oops! You didn't enter the right input!")
-                        result = get_play_again()
-                        if result:
-                            game()
-                        else:
-                            exit()
-                    except ValueError:
-                        print(
-                            "Wrong input, asking if you want to play again!")
-                        result = get_play_again()
-                        if result:
-                            game()
-                        else:
-                            exit()
+            if response_choice == 1:
+                print_sleep(
+                    "Oops, the cave exit is too far away, you're stranded!"
+                )
+                print_sleep("Enter 1 to exit cave\nEnter 2 to build a shelter")
+                response_choice = int(input("(Please enter 1 or 2.)\n"))
+                if response_choice == 1:
+                    print_sleep("The cave exit is far away, you're stranded!")
+                    get_play_again()
+                else:
+                    print_sleep("Bears found you, stuck in the cave forever!")
+                    get_play_again()
+            else:
+                print_sleep(
+                    "The bears found you, you're stuck in the cave forever!"
+                )
+                get_play_again()
 
         else:
-            print_sleep("""
-            Oops! Since the cave's exit is to far, your food ran out.
-            """)
-            result = get_play_again()
-            if result:
-                game()
-            else:
-                exit()
+            print_sleep(
+                "Oops! Since the cave's exit is to far, your food ran out."
+            )
+            get_play_again()
 # GAME FUNCTION
 
 
